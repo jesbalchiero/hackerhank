58d7c4f2b2dac05684f708d5b5e0ab193ae1071e
jesbalchiero <62852809+jesbalchiero@users.noreply.github.com>
2024-05-15 10:39:51 -0300

    divisible sum pairs in go

---
+++ divisible-sum-pairs/go.mod
@@ -0,0 +1,3 @@
+module github.com/jesbalchiero/hackerhank/divisible-sum-pairs
+
+go 1.21.6
---
+++ divisible-sum-pairs/main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'divisibleSumPairs' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER k
+ *  3. INTEGER_ARRAY ar
+ */
+
+func divisibleSumPairs(n int32, k int32, ar []int32) int32 {
+	// Initialize a variable to count the number of pairs
+	var count int32
+
+	// Iterate over the array using two nested loops
+	for i := 0; i < len(ar)-1; i++ {
+		for j := i + 1; j < len(ar); j++ {
+			// Calculate the sum of the pair
+			sum := ar[i] + ar[j]
+			// Check if the sum is divisible by k
+			if sum%k == 0 {
+				// If it is, increment the count
+				count++
+			}
+		}
+	}
+
+	// Return the count of pairs
+	return count
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	firstMultipleInput := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	nTemp, err := strconv.ParseInt(firstMultipleInput[0], 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	kTemp, err := strconv.ParseInt(firstMultipleInput[1], 10, 64)
+	checkError(err)
+	k := int32(kTemp)
+
+	arTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	var ar []int32
+
+	for i := 0; i < int(n); i++ {
+		arItemTemp, err := strconv.ParseInt(arTemp[i], 10, 64)
+		checkError(err)
+		arItem := int32(arItemTemp)
+		ar = append(ar, arItem)
+	}
+
+	result := divisibleSumPairs(n, k, ar)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
