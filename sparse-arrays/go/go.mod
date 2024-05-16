0e687d6a2832239afed562a0ebc63376abf2089e
jesbalchiero <62852809+jesbalchiero@users.noreply.github.com>
2024-05-16 17:47:15 -0300

    sparse arrays exercise in go

---
+++ sparse-arrays/go/go.mod
@@ -0,0 +1,3 @@
+module github.com/jesbalchiero/hackerhank/sparse-arrays/go
+
+go 1.21.6
---
+++ sparse-arrays/go/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+/*
+ * Complete the 'matchingStrings' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. STRING_ARRAY inputStrings
+ *  2. STRING_ARRAY queries
+ */
+
+func matchingStrings(inputStrings []string, queries []string) []int32 {
+	// Create a map to store the frequency of each string
+	frequencyMap := make(map[string]int32)
+
+	// Populate the frequency map with the input strings
+	for _, str := range inputStrings {
+		frequencyMap[str]++
+	}
+
+	// Initialize a result array to store the count of each query
+	result := make([]int32, len(queries))
+
+	// For each query, get the count from the frequency map
+	for i, query := range queries {
+		result[i] = frequencyMap[query]
+	}
+
+	return result
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 16*1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 16*1024*1024)
+
+	stringsCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+
+	var inputStrings []string
+
+	for i := 0; i < int(stringsCount); i++ {
+		stringsItem := readLine(reader)
+		inputStrings = append(inputStrings, stringsItem)
+	}
+
+	queriesCount, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+	checkError(err)
+
+	var queries []string
+
+	for i := 0; i < int(queriesCount); i++ {
+		queriesItem := readLine(reader)
+		queries = append(queries, queriesItem)
+	}
+
+	res := matchingStrings(inputStrings, queries)
+
+	for i, resItem := range res {
+		fmt.Fprintf(writer, "%d", resItem)
+
+		if i != len(res)-1 {
+			fmt.Fprintf(writer, "\n")
+		}
+	}
+
+	fmt.Fprintf(writer, "\n")
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
