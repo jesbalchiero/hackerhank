bbb32e4a15b626d71fe2cdf8b70c0f01b5489952
jesbalchiero <62852809+jesbalchiero@users.noreply.github.com>
2024-05-24 10:07:20 -0300

    feat(divisible-sum-pairs): py solution

---
+++ prepare/problem-solving-basic/divisible-sum-pairs/py/main.py
@@ -0,0 +1,20 @@
+def divisibleSumPairs(n, k, ar):
+    count = 0
+
+    for i in range(len(ar) - 1):
+        for j in range(i + 1, len(ar)):
+            if (ar[i] + ar[j]) % k == 0:
+                count += 1
+
+    return count
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+    n = int(first_multiple_input[0])
+    k = int(first_multiple_input[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    print(result)
